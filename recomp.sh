0eb0ccb3268432e979581dbc4a512e5498ee4964
pulkit <pulkitsapra97@gmail.com>
2018-05-16 19:38:38 +0530

    new commit

--- recomp.sh
+++ recomp.sh
@@ -1,6 +1,8 @@
 #!bin/sh
 
 git add .
+cd _posts
+git add .
 git commit -m "new commit"
 git push -u origin master
 git remote set-url origin https://github.com/pulkitsapra/pulkitsapra.github.io.git
